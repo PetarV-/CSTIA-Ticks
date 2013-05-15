fa21bd6ae71b241c528e900c1efa533a100694ae
Petar Veličković <pv273@cam.ac.uk>
2013-05-15 15:15:17 +0200

    Create PackedWorld.java

---
+++ Java/Tick-5/PackedWorld.java
@@ -0,0 +1,45 @@
+package uk.ac.cam.pv273.tick5;
+
+public class PackedWorld extends WorldImpl
+{
+  private long cells;
+
+  public PackedWorld()
+  {
+    super(8, 8);
+    cells = 0L;
+  }
+
+  public PackedWorld(PackedWorld prev)
+  {
+    super(prev);
+    cells = prev.cells;
+  } 
+
+  public boolean getCell(int col,int row)
+  {
+    if (col < 0 || col > 7 || row < 0 || row > 7) return false;
+    return PackedLong.get(cells,row*8 + col);
+  }
+
+  public void setCell(int col, int row, boolean alive)
+  {
+    if (col < 0 || col > 7 || row < 0 || row > 7) return;
+    cells = PackedLong.set(cells,row*8 + col,alive);
+  }
+
+  protected PackedWorld nextGeneration()
+  {
+    PackedWorld world = new PackedWorld(this);
+    int W = getWidth();
+    int H = getHeight();
+    for (int i=0;i<8;i++)
+    {
+      for (int j=0;j<8;j++)
+      {
+        world.setCell(j,i,computeCell(j,i));
+      }  
+    }
+    return world;
+  }
+}
