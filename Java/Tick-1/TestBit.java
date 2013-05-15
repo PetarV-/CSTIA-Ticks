e62965c83422e477d4dc998e16bdd327e20f0fe6
Petar Veličković <pv273@cam.ac.uk>
2013-05-15 14:57:24 +0200

    Create TestBit.java

---
+++ Java/Tick-1/TestBit.java
@@ -0,0 +1,12 @@
+package uk.ac.cam.pv273.tick1;
+
+public class TestBit
+{
+  public static void main(String[] args) throws Exception
+  {
+    long currentValue = Long.decode(args[0]);
+    int position = Integer.parseInt(args[1]);
+    boolean value = PackedLong.get(currentValue, position);
+    System.out.println(value);
+  }
+}
