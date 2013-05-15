805b69f13817c2324ff9fd0e647bedd52b74e8f2
Petar Veličković <pv273@cam.ac.uk>
2013-05-15 15:30:54 +0200

    Create PatternPanel.java

---
+++ Java/Tick-6star/PatternPanel.java
@@ -0,0 +1,34 @@
+package uk.ac.cam.pv273.tick6star;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class PatternPanel extends JPanel 
+{
+
+  private JList guiList;
+ 
+  public PatternPanel() 
+  {
+    super();
+    setLayout(new BorderLayout());
+    guiList = new JList();
+    add(new JScrollPane(guiList));
+  }
+
+  public void setPatterns(List<Pattern> list) 
+  {
+    ArrayList<String> names = new ArrayList<String>();
+
+    for (Pattern p: list)
+    {
+      names.add(p.getName() + " (" + p.getAuthor() + ")");
+    }
+
+    guiList.setListData(names.toArray());
+  } 
+}
